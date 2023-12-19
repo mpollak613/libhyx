b18b9710117bab5d191d8c6911c349da3474e22a
mpollak613 <github.h2dna@simplelogin.com>
2023-12-19 00:33:06 -0600

    fix whitespace

--- logger.h
+++ logger.h
@@ -1,5 +1,5 @@
 // <hyx/logger.h> -*- C++ -*-
-// Copyright (C) 2023  Michael Pollak
+// Copyright (C) 2023 Michael Pollak
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef HYX_LOGGER_H
 #define HYX_LOGGER_H
